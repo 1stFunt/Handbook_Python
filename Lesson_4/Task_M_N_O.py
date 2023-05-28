be780957a96d59de19b8620602c5814c0722783b
Funt <funt_88@mail.ru>
2023-05-29 01:20:01 +0300

    Вложенные циклы

--- Lesson_4/Task_M_N_O.py
+++ Lesson_4/Task_M_N_O.py
@@ -1,45 +1,45 @@
-# # M - Похожа на задачу L 
-# n = int(input())
-# m = int(input())
-# temp = 1
-# for i in range(1, n + 1):
-#     for j in range(m):
-#         if n * m < 10:
-#             print("{:1}".format(temp), end=" ")
-#         elif n * m > 10 and n * m < 100:
-#             print("{:2}".format(temp), end=" ")
-#         else:
-#             print("{:3}".format(temp), end=" ")
-#         temp += n
-#     temp = 1 + i
-#     print()
+# M - Похожа на задачу L 
+n = int(input())
+m = int(input())
+temp = 1
+for i in range(1, n + 1):
+    for j in range(m):
+        if n * m < 10:
+            print("{:1}".format(temp), end=" ")
+        elif n * m > 10 and n * m < 100:
+            print("{:2}".format(temp), end=" ")
+        else:
+            print("{:3}".format(temp), end=" ")
+        temp += n
+    temp = 1 + i
+    print()
 
-# # N - Змейка
-# n = int(input())
-# m = int(input())
-# temp = 1
-# for i in range(1, n + 1):
-#     for j in range(m):
-#         if i % 2 == 1:
-#             if n * m < 10:
-#                 print("{:1}".format(temp), end=" ")
-#             elif n * m > 10 and n * m < 100:
-#                 print("{:2}".format(temp), end=" ")
-#             else:
-#                 print("{:3}".format(temp), end=" ")
-#             if j < m - 1:
-#                 temp += 1
-#         elif i % 2 == 0:
-#             if n * m < 10:
-#                 print("{:1}".format(temp), end=" ")
-#             elif n * m > 10 and n * m < 100:
-#                 print("{:2}".format(temp), end=" ")
-#             else:
-#                 print("{:3}".format(temp), end=" ")
-#             if j < m - 1:
-#                 temp -= 1
-#     temp += m
-#     print()    
+# N - Змейка
+n = int(input())
+m = int(input())
+temp = 1
+for i in range(1, n + 1):
+    for j in range(m):
+        if i % 2 == 1:
+            if n * m < 10:
+                print("{:1}".format(temp), end=" ")
+            elif n * m > 10 and n * m < 100:
+                print("{:2}".format(temp), end=" ")
+            else:
+                print("{:3}".format(temp), end=" ")
+            if j < m - 1:
+                temp += 1
+        elif i % 2 == 0:
+            if n * m < 10:
+                print("{:1}".format(temp), end=" ")
+            elif n * m > 10 and n * m < 100:
+                print("{:2}".format(temp), end=" ")
+            else:
+                print("{:3}".format(temp), end=" ")
+            if j < m - 1:
+                temp -= 1
+    temp += m
+    print()    
 
 # O - Змейка 2.0    
 n = int(input())
